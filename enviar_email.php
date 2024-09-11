cec965b148e4e9017a0f8a5c307d821ad146e12d
lucasmorenate <lucas_smx@hotmail.com>
2024-09-11 09:26:52 -0300

    first commit

---
+++ enviar_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    $to = "lmorenate@gmail.com";
+    $subject = "Nuevo mensaje de $nombre";
+    $body = "Nombre: $nombre\nEmail: $email\n\nMensaje:\n$mensaje";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensaje enviado con éxito.";
+    } else {
+        echo "Hubo un error al enviar el mensaje.";
+    }
+}
+?>
